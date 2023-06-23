9bbe7e8ae17863617cfa1cc5105076fd5f5fc442
mmarq <mmarquez210@gmail.com>
2023-06-23 15:23:53 -0300

    added controller class and configuration database properties and entities with related fields

---
+++ src/main/java/com/portfolio/tareas/entities/ERole.java
@@ -0,0 +1,7 @@
+package com.portfolio.tareas.entities;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR
+}
---
+++ src/main/java/com/portfolio/tareas/entities/Role.java
@@ -0,0 +1,26 @@
+package com.portfolio.tareas.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role (ERole name){
+        this.name = name;
+    }
+}
